a56c31b2316c4d8fa4a0a10df59e368ca5ded6f1
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-06-10 21:35:15 +0300

    enter screen createing

--- app/src/main/java/com/music/voroshilo/activity/EnterActivity.java
+++ app/src/main/java/com/music/voroshilo/activity/EnterActivity.java
@@ -1,15 +1,72 @@
 package com.music.voroshilo.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.music.voroshilo.R;
+import com.music.voroshilo.adapter.MusicPageAdapter;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 public class EnterActivity extends AppCompatActivity {
+
+    @BindView(R.id.enter_view_pager)
+    ViewPager enterViewPager;
+
+    @BindView(R.id.enter_button)
+    Button enterButton;
+
+    @OnClick(R.id.enter_button)
+    public void setCurrentPate() {
+        int position = enterViewPager.getCurrentItem();
+        switch (position) {
+            case 0:
+            case 1:
+                enterViewPager.setCurrentItem(++position);
+                break;
+            case 2:
+                startActivity(new Intent(this, MainActivity.class));
+                this.finish();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private ViewPager.OnPageChangeListener pageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            switch (position) {
+                case 0:
+                case 1:
+                    enterButton.setText(R.string.next);
+                    break;
+                case 2:
+                    enterButton.setText(R.string.open_application);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter);
+        enterViewPager.setAdapter(new MusicPageAdapter(getSupportFragmentManager()));
+        enterViewPager.addOnPageChangeListener(pageChangeListener);
     }
 }
