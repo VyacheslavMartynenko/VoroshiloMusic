b97cdd5bbfdcd1372cb017cbb71f673e42343ee1
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-06-10 21:23:04 +0300

    enter screen createing

--- app/src/main/java/com/music/voroshilo/fragment/enter/EnterFragment.java
+++ app/src/main/java/com/music/voroshilo/fragment/enter/EnterFragment.java
@@ -1,12 +1,14 @@
 package com.music.voroshilo.fragment.enter;
 
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.music.voroshilo.R;
@@ -16,11 +18,11 @@ import butterknife.BindView;
 public class EnterFragment extends Fragment {
     public static final String POSITION = "position";
 
-    @BindView(R.id.enter_button)
-    Button enterButton;
-
     @BindView(R.id.enter_text)
     TextView enterTextView;
+
+    @BindView(R.id.container)
+    RelativeLayout containerLayout;
 
     public static EnterFragment newInstance(int position) {
 
@@ -39,10 +41,13 @@ public class EnterFragment extends Fragment {
 
         switch (position) {
             case 0:
+                setUpFragment(getString(R.string.enter_text_first), null);
                 break;
             case 1:
+                setUpFragment(getString(R.string.enter_text_second), null);
                 break;
             case 2:
+                setUpFragment(getString(R.string.enter_text_third), null);
                 break;
             default:
                 break;
@@ -51,8 +56,13 @@ public class EnterFragment extends Fragment {
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
-    public void setUpFragment() {
-        enterButton.setText();
-        enterTextView.setText();
+    @SuppressWarnings("deprecation")
+    public void setUpFragment(String text, Drawable drawable) {
+        enterTextView.setText(text);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            containerLayout.setBackground(drawable);
+        } else {
+            containerLayout.setBackgroundDrawable(drawable);
+        }
     }
 }
