3d49aaa6e39ce4cdf2918d351ed41f5360c9e9e4
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-06-10 19:50:44 +0300

    add enter activity

--- app/src/main/java/com/music/voroshilo/activity/MainActivity.java
+++ app/src/main/java/com/music/voroshilo/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.music.voroshilo;
+package com.music.voroshilo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.music.voroshilo.R;
 
 public class MainActivity extends AppCompatActivity {
 
