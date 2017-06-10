df2feec0f32a50fa8ca1b45694f3349ef7c115ec
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-06-10 22:29:19 +0300

    start creating main

--- app/src/main/java/com/music/voroshilo/activity/MainActivity.java
+++ app/src/main/java/com/music/voroshilo/activity/MainActivity.java
@@ -1,14 +1,22 @@
 package com.music.voroshilo.activity;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.music.voroshilo.R;
 
+import butterknife.BindView;
+
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.main_recycler_view)
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
