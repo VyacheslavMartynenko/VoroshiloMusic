0a4016b8b10cb3ecb18c8870c4d6cdadc80f652a
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-06-13 23:10:39 +0300

    comment

--- app/src/main/java/com/music/voroshilo/networking/DownloadService.java
+++ app/src/main/java/com/music/voroshilo/networking/DownloadService.java
@@ -1,7 +1,6 @@
 package com.music.voroshilo.networking;
 
-import com.music.voroshilo.networking.repsonse.ProgressResponseBody;
-
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Streaming;
@@ -10,6 +9,6 @@ import retrofit2.http.Url;
 public interface DownloadService {
     @Streaming
     @GET
-    Call<ProgressResponseBody> getFile(@Url String url);
+    Call<ResponseBody> getFile(@Url String url);
 
 }
