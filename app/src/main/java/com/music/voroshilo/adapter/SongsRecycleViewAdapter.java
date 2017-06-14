019b638f42212ba6523119e1502a9cbe3616b2a8
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-06-14 22:53:37 +0300

    add save file

--- app/src/main/java/com/music/voroshilo/adapter/SongsRecycleViewAdapter.java
+++ app/src/main/java/com/music/voroshilo/adapter/SongsRecycleViewAdapter.java
@@ -1,7 +1,10 @@
 package com.music.voroshilo.adapter;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,12 +12,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.music.voroshilo.R;
+import com.music.voroshilo.application.MusicApplication;
 import com.music.voroshilo.interfaces.CurrentSongListener;
+import com.music.voroshilo.interfaces.ProgressListener;
 import com.music.voroshilo.model.networking.Song;
+import com.music.voroshilo.networking.ApiBuilder;
 import com.music.voroshilo.networking.task.DownloadAsyncTask;
 import com.music.voroshilo.util.SongIconChanger;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static android.content.ContentValues.TAG;
 
 public class SongsRecycleViewAdapter extends RecyclerView.Adapter<SongsRecycleViewAdapter.SongViewHolder> {
     private List<Song> songList;
@@ -63,11 +81,11 @@ public class SongsRecycleViewAdapter extends RecyclerView.Adapter<SongsRecycleVi
             }
         };
 
-        private View.OnClickListener getDownloadClickListener(final String url) {
+        private View.OnClickListener getDownloadClickListener(final String url, final String title) {
             return new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    new DownloadAsyncTask().execute(url);
+                    downloadFile(url, title);
                 }
             };
         }
@@ -99,12 +117,81 @@ public class SongsRecycleViewAdapter extends RecyclerView.Adapter<SongsRecycleVi
             SongIconChanger.switchDrawable(context, holder.playButton, false);
         }
         holder.songTitleTextView.setText(song.getTitle());
-        holder.downloadButton.setOnClickListener(holder.getDownloadClickListener(song.getMp3Url()));
+        holder.downloadButton.setOnClickListener(holder.getDownloadClickListener(song.getMp3Url(), song.getTitle()));
         SongIconChanger.loadDrawableWithPicasso(context, holder.coverImageView, song.getImageUrl());
     }
 
     @Override
     public int getItemCount() {
         return songList.size();
+    }
+
+    private void downloadFile(String url, final String title) {
+        final ProgressListener progressListener = new ProgressListener() {
+            @Override
+            public void update(long bytesRead, long contentLength, boolean done) {
+                Log.e("R", String.valueOf(bytesRead / (double) contentLength));
+                Log.e("R", String.valueOf(done));
+            }
+        };
+
+        ApiBuilder.getDownloadService(progressListener).getFile(url).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, final Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    new AsyncTask<Void, Void, Void>() {
+                        @Override
+                        protected Void doInBackground(Void... voids) {
+                            writeResponseBodyToDisk(response.body(), title);
+                            return null;
+                        }
+                    }.execute();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e(SongsRecycleViewAdapter.this.getClass().getSimpleName(), Log.getStackTraceString(t));
+            }
+        });
+    }
+
+    private boolean writeResponseBodyToDisk(ResponseBody body, String title) {
+        try {
+            File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC), "VoroshiloMusic");
+            if (!mediaStorageDir.exists()) {
+                if (!mediaStorageDir.mkdirs()) {
+                    return false;
+                }
+            }
+            File futureStudioIconFile = new File(mediaStorageDir + File.separator + title + ".mp3");
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+            try {
+                byte[] fileReader = new byte[4096];
+                inputStream = body.byteStream();
+                outputStream = new FileOutputStream(futureStudioIconFile);
+                while (true) {
+                    int read = inputStream.read(fileReader);
+                    if (read == -1) {
+                        break;
+                    }
+                    outputStream.write(fileReader, 0, read);
+                }
+                outputStream.flush();
+                return true;
+            } catch (IOException e) {
+                return false;
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            }
+        } catch (IOException e) {
+            return false;
+        }
     }
 }
--- app/src/main/java/com/music/voroshilo/networking/task/DownloadAsyncTask.java
+++ app/src/main/java/com/music/voroshilo/networking/task/DownloadAsyncTask.java
@@ -1,34 +1,11 @@
 package com.music.voroshilo.networking.task;
 
 import android.os.AsyncTask;
-import android.util.Log;
-
-import com.music.voroshilo.interfaces.ProgressListener;
-import com.music.voroshilo.networking.ApiBuilder;
-
-import java.io.IOException;
-
-import okhttp3.ResponseBody;
-import retrofit2.Response;
 
 public class DownloadAsyncTask extends AsyncTask<String, Void, Void> {
     @Override
     protected Void doInBackground(String... strings) {
-        final ProgressListener progressListener = new ProgressListener() {
-            @Override
-            public void update(long bytesRead, long contentLength, boolean done) {
-                Log.e("R", String.valueOf(bytesRead));
-                Log.e("R", String.valueOf(contentLength));
-                Log.e("R", String.valueOf(done));
-            }
-        };
 
-        try {
-            Response<ResponseBody> response = ApiBuilder.getDownloadService(progressListener).getFile(strings[0]).execute();
-            Log.e("R", response.body().string());
-        } catch (IOException e) {
-            Log.e("R", e.toString());
-        }
 
         return null;
     }
