470c1bde9e07fbc5b807f6ea6d272f11dee7482c
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-06-28 16:38:54 +0300

    wrap card view

--- app/src/main/java/com/music/voroshilo/activity/MainActivity.java
+++ app/src/main/java/com/music/voroshilo/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SimpleItemAnimator;
@@ -17,7 +18,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
@@ -70,7 +70,7 @@ public class MainActivity extends BaseActivity implements CurrentSongListener {
     ProgressBar progressBar;
 
     @BindView(R.id.current_song_container)
-    LinearLayout currentSongContainer;
+    CardView currentSongContainer;
 
     @BindView(R.id.cover_image)
     ImageView coverImage;
