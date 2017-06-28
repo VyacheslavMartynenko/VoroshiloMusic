15bf1875ba2904462f299827ae96d75bc6e8ccff
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-06-28 20:32:01 +0300

    fix notification action

--- app/src/main/java/com/music/voroshilo/service/NotificationService.java
+++ app/src/main/java/com/music/voroshilo/service/NotificationService.java
@@ -4,7 +4,6 @@ import android.app.IntentService;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.Nullable;
-import android.util.Log;
 
 import com.music.voroshilo.model.networking.Download;
 
