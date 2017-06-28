843c2ea82c731d57e0dd7040ea50a7a0244636c4
vyacheslavmartynenko <evilacer5755g@gmail.com>
2017-06-28 13:56:43 +0300

    add crash handling

--- app/src/main/java/com/music/voroshilo/application/MusicApplication.java
+++ app/src/main/java/com/music/voroshilo/application/MusicApplication.java
@@ -2,9 +2,12 @@ package com.music.voroshilo.application;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.ads.MobileAds;
 import com.music.voroshilo.activity.BaseActivity;
 import com.music.voroshilo.util.preferences.SharedPreferencesProvider;
+
+import io.fabric.sdk.android.Fabric;
 
 public class MusicApplication extends Application {
     private static MusicApplication instance;
@@ -17,6 +20,7 @@ public class MusicApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         MobileAds.initialize(this, "ca-app-pub-3940256099942544~3347511713");
         SharedPreferencesProvider.getInstance().initialize(getApplicationContext());
         instance = this;
